b036beb00ab9b75e7718e5611d23698eb83ddba0
Karenmax11 <karen.nikoghosyan11@gmail.com>
2020-07-13 17:20:58 +0300

    11 Finding Game Objects By Name

--- 5_Realm_Rush/Assets/Pathfinder.cs
+++ 5_Realm_Rush/Assets/Pathfinder.cs
@@ -6,16 +6,25 @@ using UnityEngine;
 public class Pathfinder : MonoBehaviour
 {
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
+        ColorStartAndEnd();
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.green);
+        endWaypoint.SetTopColor(Color.red);
     }
 
     private void LoadBlocks()
     {
         var waypoints = FindObjectsOfType<Waypoint>();
+        
         foreach (Waypoint waypoint in waypoints)
         {
             var gridPos = waypoint.GetGridPos();
@@ -28,6 +37,5 @@ public class Pathfinder : MonoBehaviour
                 grid.Add(waypoint.GetGridPos(), waypoint);
             }
         }
-            print("Loaded " + grid.Count + " blocks");
     }
 }
