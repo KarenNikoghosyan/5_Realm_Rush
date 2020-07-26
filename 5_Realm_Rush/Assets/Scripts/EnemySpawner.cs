e9a053bc0a702ae1430f361d32892ab0aea5f25c
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-07-26 17:06:38 +0300

    26 Revising Coroutines

---
+++ 5_Realm_Rush/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab;
+    [SerializeField] Transform parent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true) {
+            var enemySpawn = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            enemySpawn.transform.parent = parent;
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+
+}
