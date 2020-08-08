aa93906fc9200dd0c29a11205a7fb006c11fe8d6
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-08-08 15:18:49 +0300

    37 Health For Base

---
+++ 5_Realm_Rush/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDecrease = 1;
+    [SerializeField] ParticleSystem baseExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            health = health-healthDecrease;
+        }    
+        if(health <= 0)
+        {
+            //var vfx = Instantiate(baseExplosion, transform.position, Quaternion.identity);
+            //vfx.Play();
+            //Destroy(gameObject);
+        }
+    }
+}
